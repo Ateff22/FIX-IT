606a974ce2406c6d1d176c5fcc1f6ef49f890f3e
Atef Mohamed <atef_mohamed226@outlook.com>
2026-08-10 09:15:00 +0300

    Add authRoutes and User model

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phone: { type: String },
+  specialty: { type: String },
+ role: { type: String, enum: ['customer', 'technician', 'admin'], required: true },
+isActive: { type: Boolean, default: true }, //== يشيك عالبان
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
+
---
+++ routes/authRoutes.js
@@ -0,0 +1,16 @@
+const { registerValidation, validate } = require('../middleware/validators');
+const express = require('express');
+const router = express.Router();
+const { register, login } = require('../controllers/authController');
+router.post('/login', login);
+
+router.post('/register', registerValidation, validate, register);
+
+module.exports = router;
+
+
+const { protect } = require('../middleware/authMiddleware');
+
+router.get('/me', protect, (req, res) => {
+  res.json({ message: 'You are authenticated', user: req.user });
+});
